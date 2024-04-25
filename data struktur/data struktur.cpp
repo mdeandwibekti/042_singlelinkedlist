4656ac924c26310ddd84c1ff808dde14a194c0fb
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-25 18:14:39 +0700

    KE 7

--- data struktur/data struktur.cpp
+++ data struktur/data struktur.cpp
@@ -64,6 +64,11 @@ bool serachNode(int nim, Node* current, Node* previous) {
 	previous = START;
 	current = START;
 	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
 
 
 
