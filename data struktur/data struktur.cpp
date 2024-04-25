78766b0ab448c798f34df4c923eb4e15f7951b94
mdeandwibekti <mdeandwibekti@gmail.com>
2024-04-25 18:22:05 +0700

    KE 16

--- data struktur/data struktur.cpp
+++ data struktur/data struktur.cpp
@@ -120,6 +120,17 @@ void searchData() {
 		system("cls");
 		return;
 	}
+	
+	else {
+		int nim;
+		cout << "Masukan NIM: ";
+		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+				return;
+
 
 
 
